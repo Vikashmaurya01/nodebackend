958ee604f830412ca85865f7474518466a70057d
Vikashmaurya01 <mauryavikas4665@gmail.com>
2025-04-06 23:36:06 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const app =express()
+const port = 4000
+
+app.get('/' , (req,res) =>{
+    res.send('hello world')
+})
+
+app.get('/instagram',(req,res)=>{
+    res.send('Vikash Maurya')
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>Login Here </h1>')
+})
+app.listen(process.env.PORT ,()=>{
+    console.log(`Example app ${port}`)
+})
